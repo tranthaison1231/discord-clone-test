881dad6ee7ab7dc70550dfdd498d1a1af55a58c6
Tham <pnt97test@gmail.com>
2024-02-20 19:41:21 +0700

    test api

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -5,7 +5,7 @@ import { logger } from "hono/logger";
 const app = new Hono().basePath("/api");
 
 app.use("*", logger());
-
+app.get("/" , (c) => c.json({ status: 200, message: "Success" } , 200));
 app.notFound((c) => c.json({ status: 404, message: "Not found" }, 404));
 
 serve(app, () => {
